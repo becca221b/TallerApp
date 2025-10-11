81e36bf911b258c45704638cd19f945d3b36a198
becca221b <lrpmqrp@gmail.com>
2025-10-11 10:40:59 -0300

    Just saveCustomer for now

--- domain/src/services/customer-service.ts
+++ domain/src/services/customer-service.ts
@@ -2,6 +2,6 @@ import type { Customer } from "../entities/Customer";
 
 export interface CustomerService {
   saveCustomer(customer: Customer): Promise<Customer>;
-  findCustomerByName(name: string): Promise<Customer | null>;
-  updateCustomer(id: string, customer: Partial<Customer>): Promise<Customer | null>;
+  //findCustomerByName(name: string): Promise<Customer | null>;
+  //updateCustomer(id: string, customer: Partial<Customer>): Promise<Customer | null>;
 }

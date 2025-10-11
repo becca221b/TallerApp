1122573a62c7feae32a71002f3615fb0adbf6e93
becca221b <chake_rebeca@hotmail.com>
2025-10-10 21:56:33 -0300

    just saveEmployee in employee service

--- domain/src/services/employee-service.ts
+++ domain/src/services/employee-service.ts
@@ -1,8 +1,6 @@
-import { Employee } from "../entities/Employee";
+import type { Employee } from "../entities/Employee";
 
 export interface EmployeeService {
     saveEmployee(employee: Employee): Promise<Employee>;
-    findEmployeeById(id: string): Promise<Employee | null>;
-    findEmployeeByName(name: string): Promise<Employee | null>;
-    updateEmployee(id: string, employee: Partial<Employee>): Promise<Employee | null>;
+   
 }

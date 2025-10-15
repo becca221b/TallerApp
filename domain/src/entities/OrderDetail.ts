d338febe940d614fc0ce997cee189e744392b14c
becca221b <lrpmqrp@gmail.com>
2025-10-15 20:14:34 -0300

    Make orderId optional because it will be assigned at createorder user case

--- domain/src/entities/OrderDetail.ts
+++ domain/src/entities/OrderDetail.ts
@@ -2,7 +2,7 @@ export type orderSize = 'S' | 'M' | 'L' | 'XL';
 
 export interface OrderDetail{
     id: string,
-    orderId: string,
+    orderId?: string,
     garmentId: string,
     size: orderSize,
     sex: 'F' | 'M',

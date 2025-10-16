c923863de805a513135da0d64f54e30d3b6a1058
becca221b <chake_rebeca@hotmail.com>
2025-10-16 03:13:12 -0300

    change name from services to service to unifique with the other services name

--- domain/src/services/garment-services.ts
+++
@@ -1,8 +0,0 @@
-import { Garment } from "../entities/Garment";
-
-export interface GarmentService {
-    saveGarment(garment: Garment): Promise<Garment>;
-    findGarmentById(id: string): Promise<Garment | null>;
-    //findGarmentByType(type: string): Promise<Garment | null>;
-    //updateGarment(id: string, garment: Partial<Garment>): Promise<Garment | null>;  
-}

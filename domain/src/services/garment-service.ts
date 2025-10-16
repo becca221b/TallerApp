f6fc2d6371c5265c6c102fd83b50624515837af2
becca221b <chake_rebeca@hotmail.com>
2025-10-16 03:14:20 -0300

    comment the methods that doesnt gonna use

---
+++ domain/src/services/garment-service.ts
@@ -0,0 +1,8 @@
+import { Garment } from "../entities/Garment";
+
+export interface GarmentService {
+    saveGarment(garment: Garment): Promise<Garment>;
+    findGarmentById(id: string): Promise<Garment | null>;
+    //findGarmentByType(type: string): Promise<Garment | null>;
+    //updateGarment(id: string, garment: Partial<Garment>): Promise<Garment | null>;  
+}

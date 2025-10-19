ed8aa82c4aab070f52f7f08fe6eb50d96d9c0ed2
becca221b <chake_rebeca@hotmail.com>
2025-10-19 04:20:14 -0300

    Add User schema

---
+++ apps/backend/src/models/UserModel.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+import { User } from '@/domain/entities/User';
+
+const UserSchema = new Schema({
+    username: { 
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 50
+    },
+    passwordHash: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'employee'], required: true },
+    email: { 
+        type: String,
+        required: false,
+        unique: true,
+        sparse: true,
+        lowercase: true,
+        trim: true
+    },
+});
+
+export const UserModel = model('User', UserSchema);

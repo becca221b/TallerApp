16f4538a900f59d2368c93718e4df14f86399b68
becca221b <chake_rebeca@hotmail.com>
2025-10-20 11:01:33 -0300

    Server

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,14 +1,9 @@
 import express from "express";
 import cors from "cors";
-require('dotenv').config();
-import { authRoutes } from "./routes/auth.routes";
-import { orderRoutes } from "./routes/order.routes";
-import { customerRoutes } from "./routes/customer.routes";
-import { authController } from "./controllers/auth.controller";
-import { orderController } from "./controllers/order.controller";
-import { customerController } from "./controllers/customer.controller";
-import { authMiddleware } from "./middlewares/auth.middleware";
-import { connectDB } from "./config/db";
+import authRoutes from "./routes/auth.routes";
+//import { orderRoutes } from "./routes/order.routes";
+//import { customerRoutes } from "./routes/customer.routes";
+
 
 const app = express();
 
@@ -16,32 +11,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectDB(); // connect to Mongo
-
-  // Initialize repositories
-  const userRepository = new UserRepository();
-  const customerRepository = new CustomerRepository();
-  const orderRepository = new OrderRepository();
-
-  // Initialize services
-  const authService = new AuthService(userRepository);
-
-  // Initialize controllers
-  const authController = new AuthController(authService);
-  const orderController = new OrderController(orderRepository, customerRepository);
-  const customerController = new CustomerController(customerRepository);
-
-  // Initialize middleware
-  const authMiddleware = new AuthMiddleware(authService, userRepository);
 
 //Routes
-app.use("/api/auth", authRoutes(authController));
-app.use("api/orders", orderRoutes(orderController, authMiddleware));
-app.use("api/customers", customerRoutes(customerController, authMiddleware));
+app.use("/api/auth", authRoutes);
+//app.use("api/orders", orderRoutes);
+//app.use("api/customers", customerRoutes);
 
-
+//Test endpoint
+app.get("/", (req, res) => res.json({ ok: true, message: "Backend de TallerApp" }));
 // Middleware error 404 for any other routes
-app.use('*',(req, res) => {
+app.use((req, res) => {
     res.status(404).json({ error: 'Ouch!! That route does not exist.' });
 });
 
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,6 +1,12 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import { app } from "./app";
+import { connectDB } from "./config/db";
+
+dotenv.config();
+
 const PORT = process.env.PORT || 4000;
+
+connectDB(); // connect to Mongo
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

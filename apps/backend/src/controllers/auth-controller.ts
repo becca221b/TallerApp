21ef290f10ae46d244f8ca73e6a22bfc710191b6
becca221b <lrpmqrp@gmail.com>
2025-10-21 20:55:08 -0300

    Fix auth controller

--- apps/backend/src/controllers/auth-controller.ts
+++ apps/backend/src/controllers/auth-controller.ts
@@ -1,29 +1,28 @@
 import { Request, Response } from "express";
 import { RegisterUser } from "@/domain/use-cases/register-user";
 import { LoginUser } from "@/domain/use-cases/login-user";
-import { UserRepository } from "../repositories/UserRepository";;
 import { generateToken } from "../config/jwt";
 
-const userRepository = new UserRepository();
-
 export class AuthController {
+
+  constructor (private registerUser: RegisterUser, private loginUser: LoginUser) {}
   
-  static async register(req: Request, res: Response) {
+  async register(req: Request, res: Response):Promise<void> {
     try {
       const { username, password, role } = req.body;
-      const registerUser = new RegisterUser(userRepository);
-      const user = await registerUser.execute({ username, password, role });
+     
+      const user = await this.registerUser.execute({ username, password, role });
       res.status(201).json({ message: "Usuario registrado exitosamente", user });
     } catch (error: any) {
       res.status(400).json({ message: error.message });
     }
   }
 
-  static async login(req: Request, res: Response) {
+  async login(req: Request, res: Response):Promise<void> {
     try {
       const { username, password } = req.body;
-      const loginUser = new LoginUser(userRepository);
-      const user = await loginUser.execute({ username, password });
+   
+      const user = await this.loginUser.execute({ username, password });
       const token = generateToken(user);
       res.status(200).json({ message: "Login exitoso", token });
     } catch (error: any) {

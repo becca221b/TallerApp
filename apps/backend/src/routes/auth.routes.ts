1f6151fb9a9c8bbbb5b36e06c1d369c9f9872b10
becca221b <lrpmqrp@gmail.com>
2025-10-22 17:55:05 -0300

    adapt to new contstructor

--- apps/backend/src/routes/auth.routes.ts
+++ apps/backend/src/routes/auth.routes.ts
@@ -1,16 +1,13 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth-controller";
 import { UserRepository } from "../repositories/UserRepository";
-import { RegisterUser } from "@/domain/use-cases/register-user"
-import { LoginUser } from "@/domain/use-cases/login-user";
+
 
 const router = Router();
 
 // Inyección manual de dependencias
 const userRepository = new UserRepository();
-const loginUseCase = new LoginUser(userRepository);
-const registerUseCase = new RegisterUser(userRepository);
-const authController = new AuthController(registerUseCase, loginUseCase);
+const authController = new AuthController(userRepository);
 
 
 router.post("/register", (req, res)=> authController.register(req, res));

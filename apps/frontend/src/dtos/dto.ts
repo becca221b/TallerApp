d37a8952c402767933ee339763ec5d04d0865f31
Rebeca <chake_rebeca@hotmail.com>
2025-11-12 00:44:45 -0300

    Add dtos

---
+++ apps/frontend/src/dtos/dto.ts
@@ -0,0 +1,46 @@
+export interface User{
+    id: string;
+    username: string,
+    role: 'Costurero' | 'Supervisor';
+    token?: string;
+}
+
+export interface Customer{
+    id:string;
+    name: string;
+    email?: string;
+    phone?: string;
+}
+
+export interface Garment {
+  garmentId: string;
+  quantity: number;
+  price: number;
+  size: string;
+  sex: 'M' | 'F' | 'U';
+  subtotal: number;
+}
+
+export interface Order {
+    id: string;
+    customerId: string;
+    employeeId: string;
+    customerName?: string;
+    employeeName?: string;
+    orderDetails: Garment[];
+    totalPrice: number;
+    deliveryDate: string;
+    createdAt: string;
+    updatedAt?: string;
+    status: 'pending' | 'in process' | 'completed';
+}
+
+export interface Employee {
+    id: string;
+    name: string;
+    email?: string;
+    phone?: string;
+    employeeType: 'Costurero' | 'Supervisor';
+    username: string;
+    password: string;
+}

7c57e25c143b7ca94941391b0df1a540c1a88aac
Rebeca <chake_rebeca@hotmail.com>
2025-11-22 16:40:47 -0300

    change MONGO_URI acording to secrets

--- apps/backend/src/config/db.ts
+++ apps/backend/src/config/db.ts
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import fs from "fs";
 
 dotenv.config();
 
 export async function connectDB() {
-    if (!process.env.MONGO_DB_URI) {
-        throw new Error('MONGO_URI is not defined in your .env file');
-    }
+    const mongoUriPath = process.env.MONGO_URI_FILE || "";
+    const mongoUri = fs.readFileSync(mongoUriPath, "utf8");
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI);
+        await mongoose.connect(mongoUri);
         console.log("Connected to MongoDB");
     }catch (error) {
         console.error("Error connecting to MongoDB:", error);
